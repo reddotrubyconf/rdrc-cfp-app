ab0fd8973809f6354740cabaffe0669ec24d5d50
Ted Johansson <drenmi@gmail.com>
2016-12-17 00:01:27 +0800

    Validate presence of User bio when updating profile
    Previously we didn't validate the presence of a bio when a speaker
    updated their profile. This change fixes that.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_uniqueness_of(:email) }
+  it { is_expected.to validate_presence_of(:bio).on(:update) }
 
   describe ".create_from_auth" do
     context "with valid attributes" do
